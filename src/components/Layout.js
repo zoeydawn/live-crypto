2564275fc080de61e51ad421129ba55591b81770
Donovan <donovan6925@gmail.com>
2017-11-11 18:05:28 -0500

    updated favicon and title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Icon } from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react';
 
 import { subscribeToBfx, unsubscribeToBfx } from '../actions';
 
