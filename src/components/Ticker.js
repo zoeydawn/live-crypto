c128d248d07fd38e75164dbb35db72a1ee779eec
Donovan <donovan6925@gmail.com>
2017-11-19 17:25:41 -0500

    removed bundle because it was causing errors with webpack

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,25 +1,32 @@
 import React, { PropTypes } from 'react';
 import { Icon } from 'semantic-ui-react';
 
-const Ticker = ({ ticker }) => (
-  <div className="ticker">
-    <div className="left-tick">
-      <h1>BTC/USD</h1>
-      <p>
-        <span id="last-price">{ticker[6]}{' '}{' '}</span>
-        <Icon name={ticker[5] > 0 ? 'arrow up' : 'arrow down'} />
-        {(Math.round(ticker[5] * 1000) / 10)}%
-      </p>
-      <p>vol: {ticker[7]}</p>
+const Ticker = ({ ticker }) => {
+  if (!ticker.length) {
+    return null;
+  }
+
+  return (
+    <div className="ticker">
+      <div className="left-tick">
+        <h1>BTC/USD</h1>
+        <p>
+          <span id="last-price">{ticker[6]}{' '}{' '}</span>
+          <Icon name={ticker[5] > 0 ? 'arrow up' : 'arrow down'} />
+          {(Math.round(ticker[5] * 1000) / 10)}%
+        </p>
+        <p>vol: {ticker[7]}</p>
+      </div>
+      <div className="right-tick">
+        <p>high: {ticker[8]}</p>
+        <p>low: {ticker[9]}</p>
+        <p>bid: {ticker[0]}</p>
+        <p>ask: {ticker[2]}</p>
+      </div>
     </div>
-    <div className="right-tick">
-      <p>high: {ticker[8]}</p>
-      <p>low: {ticker[9]}</p>
-      <p>bid: {ticker[0]}</p>
-      <p>ask: {ticker[2]}</p>
-    </div>
-  </div>
-);
+  );
+}
+
 
 Ticker.propTypes = {
   ticker: PropTypes.array.isRequired,
